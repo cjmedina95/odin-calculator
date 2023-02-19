de2242a56282b30e82ba76a3ef37c78462971999
Christian <Christian.Medina.ATX@gmail.com>
2023-02-18 23:07:18 -0600

    Add partial keyboard input functionality

--- js/main.js
+++ js/main.js
@@ -50,3 +50,36 @@ numBtn.forEach((number) => {
 decimal.addEventListener("click", setDecimal);
 deleteButton.addEventListener("click", backspace);
 clearEntryButton.addEventListener("click", clearEntry);
+
+document.addEventListener('keydown', (e) => {
+    if (e.repeat) return;
+
+    const key = e.key;
+    console.log(key);
+    switch (key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if (currentEntry === "0") {
+                currentEntry = key;
+            }
+            else {
+                currentEntry = currentEntry.concat(key);
+            }
+            break;
+        case 'Backspace':
+            backspace();
+            break;
+        default:
+            console.log("wtf");
+    }
+
+    renderText();
+});
