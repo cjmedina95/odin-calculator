53794870c346ed75fb5d333832c7d0a09ca46826
Christian <Christian.Medina.ATX@gmail.com>
2023-02-18 22:35:34 -0600

    Add number entry functionality

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
-var currentEntry = [0.3];
+var currentEntry = "0";
 
 const deleteButton = document.querySelector("button#delete");
 const clearEntryButton = document.querySelector("button#clearEntry");
+const numBtn = [];
 
 function renderText() {
     const answerText = document.querySelector("#answerFrame > p");
-    answerText.innerText = currentEntry.join("");
+    answerText.innerText = currentEntry;
 }
 
 function backspace() {
-    currentEntry.pop();
+    currentEntry = currentEntry.slice(0, -1);
     if (currentEntry.length === 0) {
-        currentEntry.push(0);
+        currentEntry = "0";
     }
     renderText();
 }
 
 function clearEntry() {
-    currentEntry = [0];
+    currentEntry = "0";
     renderText();
 }
 
+for (let i = 0; i < 10; i++) {
+    numBtn.push(document.querySelector("button#btn" + i));
+}
+
+numBtn.forEach((number) => {
+    number.addEventListener("click", function(){
+        if (currentEntry === "0") {
+            currentEntry = number.innerText;
+        }
+        else {
+            currentEntry = currentEntry.concat(number.innerText);
+        }
+        renderText();
+    });
+});
+
 deleteButton.addEventListener("click", backspace);
 clearEntryButton.addEventListener("click", clearEntry);
-renderText();
